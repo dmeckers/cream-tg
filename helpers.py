2dd297d590367b416684eb2117c59dfbc6c05122
dmeckers <morph232@inbox.lv>
2024-09-07 04:37:29 +0300

    Refactor Dockerfile CMD to use ENTRYPOINT instead of CMD

--- helpers.py
+++ helpers.py
@@ -1,10 +1,18 @@
 import os
+from time import sleep
 import requests
 from pathlib import Path
 from urllib.parse import urlparse
+import keyring
+import keyrings.alt
 
 
 class TgBotHelpers:
+
+    _SERVICE_NAME = "TgBotService"
+    _TOKEN_KEY = "jwt_token"
+    _SERVER_DOMAIN = "https://cream-api"
+    _TOKEN_FILE_PATH = Path(__file__).resolve().parent / "jwt_token.txt"
 
     @staticmethod
     async def download_audio_file(update, context) -> dict:
@@ -51,3 +59,58 @@ class TgBotHelpers:
                 f"Failed to download file. Status code: {response.status_code}, Response text: {response.text}"
             )
             response.raise_for_status()
+
+    @staticmethod
+    def login_on_401_and_upload_file(file_name, file, local_file_path):
+        token = TgBotHelpers.getOrFetchJwtToken()
+        headers = {
+            "Accept": "/json",
+            "Authorization": f"Bearer {token}",
+        }
+
+        files = {"file": (file_name, file)}
+
+        try:
+            requests.post(
+                f"{TgBotHelpers._SERVER_DOMAIN}/api/v1/tracks",
+                headers=headers,
+                files=files,
+                verify=False,
+            )
+        except Exception as e:
+            os.remove(local_file_path)
+            raise e
+
+        os.remove(local_file_path)
+
+    @staticmethod
+    def getOrFetchJwtToken() -> str:
+        """
+        Fetches the JWT token from the text file or fetches a new one from the server.
+        :returns: JWT token
+        :raises Exception: If the login fails
+        """
+        if TgBotHelpers._TOKEN_FILE_PATH.exists():
+            with open(TgBotHelpers._TOKEN_FILE_PATH, "r") as token_file:
+                token = token_file.read().strip()
+                if token:
+                    return token
+
+        response = requests.post(
+            f"{TgBotHelpers._SERVER_DOMAIN}/api/login",
+            json={
+                "email": os.getenv("BOT_WORKER_EMAIL"),
+                "password": os.getenv("BOT_WORKER_PASSWORD"),
+            },
+            verify=False,
+        )
+
+        if response.status_code != 200 and response.status_code != 201:
+            raise Exception("Failed to login")
+
+        new_token = response.json()["token"]
+
+        with open(TgBotHelpers._TOKEN_FILE_PATH, "w") as token_file:
+            token_file.write(new_token)
+
+        return new_token
--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import re
 # Logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 logger = logging.getLogger(__name__)
 
@@ -25,50 +26,14 @@ TOKEN = os.getenv("BOT_TOKEN", "too sad for you :(")
 WEB_APP_URL = os.getenv("WEB_APP_URL", "https://dmeckers.github.io/cream-front/")
 BOT_SUPERADMIN_ID = int(os.getenv("BOT_SUPERADMIN_ID", 0))
 
-# Global variables
+BOT_WORKER_EMAIL = os.getenv("BOT_WORKER_EMAIL", "cream_radio_bot")
+BOT_WORKER_PASSWORD = os.getenv("BOT_WORKER_PASSWORD", "cream_radio_bot")
 stations = ["cream"]
 
-async def start_command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+async def defaultGreetings(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Welcome",
-        reply_markup=ReplyKeyboardMarkup(
-            [
-                [
-                    KeyboardButton(
-                        "Open Web App",
-                        web_app=WebAppInfo(WEB_APP_URL),
-                    )
-                ]
-            ],
-            resize_keyboard=True,
-        ),
-    )
-
-# Handlers
-async def text_message_handler(
-    update: Update, context: ContextTypes.DEFAULT_TYPE
-) -> None:
-    message_text = update.message.text
-    match_add = re.match(r"^<(.+)>$", message_text)
-    match_remove = re.match(r"^>(.+)<$", message_text)
-
-    if match_add:
-        station_name = match_add.group(1)
-        stations.append(station_name)
-        await update.message.reply_text(f"Station {station_name} has been created.")
-        return
-
-    if match_remove:
-        station_name = match_remove.group(1)
-        if station_name in stations:
-            stations.remove(station_name)
-            await update.message.reply_text(f"Station {station_name} has been removed.")
-        else:
-            await update.message.reply_text(f"Station {station_name} does not exist.")
-        return
-
-    await update.message.reply_text(
-        "Wassup ma nigga, hope u doin good. I'm just a fucking piece of hardcoded message. Hit the button below to open the radio.",
+        "Welcome to Cream Radio! Hit the button below to open the radio.",
         reply_markup=ReplyKeyboardMarkup(
             [[KeyboardButton("Open Web App", web_app=WebAppInfo(WEB_APP_URL))]],
             resize_keyboard=True,
@@ -76,16 +41,23 @@ async def text_message_handler(
     )
 
 
+async def start_command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await defaultGreetings(update, context)
+
+
+async def text_message_handler(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    await defaultGreetings(update, context)
+
+
 async def audio_message_handler(
     update: Update, context: ContextTypes.DEFAULT_TYPE
 ) -> None:
-    domain = "https://cream-api"
-
     sender_id = update.message.from_user.id
+
     if sender_id != BOT_SUPERADMIN_ID:
-        return await context.message.reply_text(
-            "Only superadmin can upload audio files.505ATb"
-        )
+        return await context.message.reply_text("Ti ohuel")
 
     try:
         [local_file_path, file_name] = await TgBotHelpers.download_audio_file(
@@ -93,36 +65,16 @@ async def audio_message_handler(
         )
 
         with open(local_file_path, "rb") as file:
-            files = {"file": (file_name, file)}
+            TgBotHelpers.login_on_401_and_upload_file(file_name, file, local_file_path)
 
-            response = requests.post(
-                f"{domain}/api/v1/tracks",
-                headers={
-                    "Accept": "application/json",
-                    "Authorization": f"Bearer {os.getenv('JWT_TOKEN')}",
-                },
-                files=files,
-                verify=False,
-            )
+        await update.message.reply_text("Audio has been uploaded.")
 
-            os.remove(local_file_path)
-
-            if response.status_code == 201:
-                await update.message.reply_text("Audio has been uploaded.")
-            else:
-                await update.message.reply_text("Error when uploading file")
-                logger.error(
-                    f"Upload failed with status {response.status_code}: {response.text}"
-                )
     except Exception as e:
         logger.error(f"Error occurred: {str(e)}")
         await update.message.reply_text(f"Error when uploading file {str(e)}")
 
 
 def main() -> None:
-
-    if not os.getenv("JWT_TOKEN"):
-        raise Exception("JWT_TOKEN is not set")
 
     application = ApplicationBuilder().token(TOKEN).build()
 
